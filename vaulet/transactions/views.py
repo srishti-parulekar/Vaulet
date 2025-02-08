1cc45ba9743dfde087f09b6e21ffdd1dd54645cc
Srishti Parulekar <srishtiparulekar430@gmail.com>
2025-02-09 00:55:54 +0530

    transactions models and views in new app

--- vaulet/transactions/views.py
+++ vaulet/transactions/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics, filters
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+from .serializers import (
 
-# Create your views here.
+    TransactionSerializer
+)
+from .models import Transaction
+from django_filters.rest_framework import DjangoFilterBackend
+
+class TransactionListView(generics.ListAPIView):
+    serializer_class = TransactionSerializer
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ['transaction_type', 'challenge', 'vault']
+    ordering_fields = ['created_at', 'amount']
+    ordering = ['-created_at']
+
+    def get_queryset(self):
+        return Transaction.objects.filter(user=self.request.user)
+
