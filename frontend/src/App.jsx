171795ea897d813f1377a70dfd23e3b1cc863007
Srishti Parulekar <srishtiparulekar430@gmail.com>
2025-05-22 19:36:22 +0530

    gh-pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
@@ -23,7 +23,7 @@ function App() {
 
   return (
     <>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/home" element={
             <ProtectedRoutes>
@@ -40,7 +40,7 @@ function App() {
 
         </Routes>
 
-      </BrowserRouter>
+      </HashRouter>
     </>
   )
 }
